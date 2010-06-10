9a290dd590c9d5e24cc58679908d47de2d1869e6
Calvin Yu <csyu77@gmail.com>
2010-06-10 14:53:14 -0400

    added delete/write/mkdir functionality

--- files.rb
+++ files.rb
@@ -1,16 +1,36 @@
 require 'rubygems'
 require 'sinatra'
-require '../rack-cors/lib/rack/cors'
+require 'rack/cors'
 require 'yaml'
 require 'yajl'
 require 'dir_walker'
+require 'fileutils'
 
-use Rack::Cors do |origins|
-  origins.allow ['localhost:3000', '127.0.0.1:3000'], '/file/list_all/', :allow_headers => 'x-domain-token'
-  origins.allow ['localhost:3000', '127.0.0.1:3000'], '/file/at', :methods => [:get, :post, :put, :delete], :allow_headers => 'x-domain-token'
+use Rack::Cors do |config|
+  config.allow do |allow|
+    # allow.origins 'localhost:3000', '127.0.0.1:3000'
+    allow.origins '*'
+    allow.resource '/file/list_all/', :headers => :any
+    allow.resource '/file/at/*',
+        :methods => [:get, :post, :put, :delete],
+        :headers => :any,
+        :max_age => 0
+  end
 end
 
 set :mount_points, YAML.load_file(File.join(File.dirname(__FILE__), 'mount_points.yml'))
+
+helpers do
+  def mount_point
+    path = params[:splat].first
+    path[0..path.index('/')-1]
+  end
+
+  def translated_path
+    path = params[:splat].first
+    File.join(options.mount_points[mount_point], path[path.index('/')..-1])
+  end
+end
 
 get '/file/list_all/' do
   headers(
@@ -29,11 +49,19 @@ get '/file/list_all/' do
 end
 
 get '/file/at/*' do
-  path = params[:splat].first
-  mount_point = path[0..path.index('/')-1]
-
   file = Rack::File.new('')
-  file.path = File.join(options.mount_points[mount_point], path[path.index('/')..-1])
+  file.path = translated_path
   file
 end
 
+put '/file/at/*' do
+  if request.body.length == 0
+    FileUtils.mkdir_p translated_path
+  else
+    File.open(translated_path, 'w') {|f| FileUtils.copy_stream(request.body, f)}
+  end
+end
+
+delete '/file/at/*' do
+  FileUtils.rm translated_path
+end
